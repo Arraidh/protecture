d3c23873290710fdb2ed52649d137556af4f5868
Arraidh <ari.darmika@gmail.com>
2023-06-12 02:53:14 +0700

    make nav button to success when clicked

--- front-end/src/component/sidebar.js
+++ front-end/src/component/sidebar.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../App.css";
 import * as Icons from "react-bootstrap-icons";
 
 function Sidebar({ isOpen }) {
+  const [activeButton, setActiveButton] = useState("laporanBtn");
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
   return (
     <div className={`Sidebar shadow-lg ${isOpen ? "" : "d-none"}`}>
       <nav className="row p-3 m-0 shadow-sm">
-        <button className="navItem btn btn-success d-flex flex-column align-items-center justify-content-center col h-100 w-100 p-0">
+        <button
+          className={`navItem btn ${
+            activeButton === "laporanBtn" ? "btn-success" : "btn-light"
+          } d-flex flex-column align-items-center justify-content-center col h-100 w-100 p-0`}
+          onClick={() => handleButtonClick("laporanBtn")}
+        >
           <Icons.Geo size={36} />
           Laporan
         </button>
-        <button className="navItem btn btn-light d-flex flex-column align-items-center justify-content-center col h-100 w-100 p-0">
+        <button
+          className={`navItem btn ${
+            activeButton === "volunteerBtn" ? "btn-success" : "btn-light"
+          } d-flex flex-column align-items-center justify-content-center col h-100 w-100 p-0`}
+          onClick={() => handleButtonClick("volunteerBtn")}
+        >
           <Icons.PeopleFill size={36} />
           Volunteer
         </button>
-        <button className="navItem btn btn-light d-flex flex-column align-items-center justify-content-center col h-100 w-100 p-0">
+        <button
+          className={`navItem btn ${
+            activeButton === "donationBtn" ? "btn-success" : "btn-light"
+          } d-flex flex-column align-items-center justify-content-center col h-100 w-100 p-0`}
+          onClick={() => handleButtonClick("donationBtn")}
+        >
           <Icons.CashCoin size={36} />
           Donation
         </button>
