836fc2fc3a42e2ca8df46a05e24c6ab907149fa3
Arraidh <ari.darmika@gmail.com>
2023-06-12 21:43:34 +0700

    Add Donation Card

--- front-end/src/component/donationCard.js
+++ front-end/src/component/donationCard.js
@@ -5,8 +5,8 @@ import { Button } from "bootstrap";
 
 const DonationCard = () => {
   return (
-    <div className="card gap-3 shadow">
-      <div class="card-body gap-2 p-3 d-flex flex-column">
+    <div className="card shadow">
+      <div class="card-body gap-2 p-4 d-flex flex-column">
         <div>
           <h3>Kebakaran Hutan</h3>
           <p className="d-inline m-0">
@@ -14,11 +14,17 @@ const DonationCard = () => {
             <span className="badge text-bg-success m-2">Kebakaran</span>
           </p>
         </div>
-        <div className="d-flex align-items-center gap-2">
-          <h6>Donasi Konservasi</h6> <span>Tersisa 9 jam</span>
+        <div className="d-flex align-items-center gap-2 justify-content-between ">
+          <p className="fs-5 fw-bolder mb-0">Donasi Konservasi </p>
+          <p className="fs-6 mb-0">Tersisa 9 jam</p>
         </div>
-        <div className="d-flex align-items-center gap-2"></div>
-        <div className="d-flex align-items-center gap-2  justify-content-between">
+        <div className="d-flex align-items-center gap-2 justify-content-between">
+          <p className="fs-4 fw-bolder">
+            Rp 2.000.000 <span className="text-success fs-6">Terkumpul</span>
+          </p>
+          <p className="fs-6">Berakhir pada 30/05/2023</p>
+        </div>
+        <div className="d-flex align-items-center gap-2  justify-content-end">
           <button
             type="button"
             class="btn btn-success gap-2 d-flex align-items-center"
