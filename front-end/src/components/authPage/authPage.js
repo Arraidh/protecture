076bf09de518bff59ff3d73cb37330bca34305f8
Arraidh <ari.darmika@gmail.com>
2023-06-17 15:03:54 +0700

    Connect Login API to Front-End

--- front-end/src/components/authPage/authPage.js
+++ front-end/src/components/authPage/authPage.js
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 import AuthVector from "../../images/AuthVector-01.png";
 import LoginPage from "./loginPage";
 import RegisterPage from "./registerPage";
+import { AuthProvider } from "../../utils/useAuth";
 
 const AuthPage = (props) => {
   const [isLogin, setIsLogin] = useState(props.isLogin);
-
-  console.log(props);
 
   return (
     <div className="loginPage d-flex align-items-center justify-content-center">
       <div className="container">
         <div className="card p-5 gap-3 d-flex flex-row align-items-center">
           {isLogin ? (
-            <LoginPage authState={isLogin} setAuthState={setIsLogin} />
+            <AuthProvider>
+              <LoginPage authState={isLogin} setAuthState={setIsLogin} />
+            </AuthProvider>
           ) : (
             <RegisterPage />
           )}
