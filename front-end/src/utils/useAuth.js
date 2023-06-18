91aa2be5e92807dcad3dd77635f5886e8b7a39af
Arraidh <ari.darmika@gmail.com>
2023-06-19 06:19:46 +0700

    Resolve some logout bug

--- front-end/src/utils/useAuth.js
+++ front-end/src/utils/useAuth.js
@@ -24,6 +24,7 @@ const AuthProvider = ({ children }) => {
     setIsLoggedIn(false);
     setUserData(null);
     localStorage.removeItem("userData");
+    localStorage.removeItem("isLoggedIn");
   };
 
   return (
